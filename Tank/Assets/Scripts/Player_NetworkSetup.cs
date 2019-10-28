7b34937e14f47f76e74d72a60c716a85f47a4667
Mat <mat555333@gmail.com>
2019-10-28 15:06:49 +0100

    added basic networking
    i don't know why all these files change. what the fuck

---
+++ Tank/Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player_NetworkSetup : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (isLocalPlayer)
+        {
+            GetComponent<TankMovement>().enabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+            //GetComponentInChildren<Turret>().enabled = true;
+            //GetComponentInChildren<AudioListener>().enabled = true;
+        }
+    }
+
+
+}
