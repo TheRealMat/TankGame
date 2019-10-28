881d2ce2ed8b4c5c3c9dac817db41423e71410ce
Mat <mat555333@gmail.com>
2019-10-28 09:52:22 +0100

    init

---
+++ Tank/Assets/Scripts/TankMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float thrustForward = 50.0F;
+    private float thrustBackward = 20.0F;
+    private float rotationSpeed = 2.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb2D.AddForce(transform.up * thrustForward);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb2D.AddForce(transform.up * -thrustBackward);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb2D.rotation += rotationSpeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            rb2D.rotation -= rotationSpeed;
+        }
+
+    }
+}
---
+++ Tank/Assets/Scripts/Turret.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var dir = point - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+    }
+}
