9c862e3816f34f96f320a1314d5b70ec6c1564a5
Mat <mat555333@gmail.com>
2019-10-30 09:47:47 +0100

    added motion sickness

---
+++ Tank/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = Vector3.MoveTowards(new Vector3(transform.parent.position.x, transform.parent.position.y, -20), new Vector3(point.x, point.y, -20) , 10);
+    }
+}
