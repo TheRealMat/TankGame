349b4779f2b678a7988f9d68b405d491d6beda22
Mat <mat555333@gmail.com>
2019-10-30 08:36:35 +0100

    added physics
    they are janky but they are there

--- Tank/Assets/Scripts/TankMovement.cs
+++ Tank/Assets/Scripts/TankMovement.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class TankMovement : MonoBehaviour
 {
     private Rigidbody2D rb2D;
-    private float thrustForward = 50.0F;
+    private float thrustForward = 70.0F;
     private float thrustBackward = 20.0F;
-    private float rotationSpeed = 2.0F;
+    private float rotationSpeed = 15.0F;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,11 +27,11 @@ public class TankMovement : MonoBehaviour
         }
         if (Input.GetKey("a"))
         {
-            rb2D.rotation += rotationSpeed;
+            rb2D.AddTorque(rotationSpeed, ForceMode2D.Force);
         }
         if (Input.GetKey("d"))
         {
-            rb2D.rotation -= rotationSpeed;
+            rb2D.AddTorque(-rotationSpeed, ForceMode2D.Force);
         }
 
     }
