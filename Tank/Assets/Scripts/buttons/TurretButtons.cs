cd6961bcb0b830b30f532bc797a2a9dabe8b903a
Mat <mat555333@gmail.com>
2019-10-31 11:48:34 +0100

    sprites and misc

--- Tank/Assets/Scripts/buttons/TurretButtons.cs
+++ Tank/Assets/Scripts/buttons/TurretButtons.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretRight : MonoBehaviour
+public class TurretButtons : MonoBehaviour
 {
     public int value;
     public void DoTheThing()
